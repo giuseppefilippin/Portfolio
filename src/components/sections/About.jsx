51c0dcf0220f2ff069f9adc7710e0a06bcd67b13
Giuseppe Filippin <giuseppe.ferreira@pucpr.edu.br>
2025-02-28 16:45:06 -0300

    creating about me  section

---
+++ src/components/sections/About.jsx
@@ -0,0 +1,102 @@
+export const About = () => {
+  const frontendSkills = [
+    "HTML5",
+    "CSS3",
+    "JavaScript",
+    "React",
+    "Tailwind CSS",
+  ];
+  const backendSkills = [
+    "Node.js",
+    "Express",
+    "MongoDB",
+    "Python",
+    "MySQL",
+    "PHP",
+    "Rust",
+    "Java",
+  ];
+  return (
+    <section
+      id="about"
+      className="min-h-screen flex items-center justify-center py-20"
+    >
+      <div className="max-w-3xl mx-auto px-4">
+        <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+          About Me
+        </h2>
+        <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+          <p className="text-gray-300 mb-6">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, ullam
+            nulla culpa et, alias fugiat nihil odit aliquid impedit incidunt
+            similique iste distinctio ducimus est hic dicta non. Accusamus,
+            deleniti!
+          </p>
+          <div className="grid gird-cols-1 md:grid-cols-2 gap-6">
+            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4">FrontEnd</h3>
+              <div className="flex flex-wrap gap-2">
+                {frontendSkills.map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 px-3 py-1 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+            </div>
+            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4">BackEnd</h3>
+              <div className="flex flex-wrap gap-2">
+                {backendSkills.map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 px-3 py-1 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
+          <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+            <h3 className="text-xl font-bold mb-4"> 📚 Education </h3>
+            <ul className="list-disc list-inside text-gray-300 space-y-2">
+              <li>
+                <strong>English Diploma</strong> Phill Young`s
+              </li>
+              <li>
+                <strong>High School Diploma</strong> Colegio Marista Santa
+                Maria(2018-2021)
+              </li>
+              <li>
+                <strong>B.S. in Computer Science </strong> PUCPR
+                University(2023-2026)
+              </li>
+              <li>
+                Relevant Coursework: Data Structures, Algorithms, Web
+                Development, Databases
+              </li>
+            </ul>
+          </div>
+          <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+            <h3 className="text-xl font-bold mb-4"> 💼 Work Experience </h3>
+            <div className="space-y-4 text-gray-300">
+                {/* copiar a Div abaixo para adicionar futuras work experience */}
+              <div> 
+                <h4 className="font-bold">Bolsista PIBITI / PIBIC at Lactec (2025-Present)</h4>
+                <p>
+                  Developing a TRL calculator website as a scientific initiation
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
