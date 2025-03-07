f545d5caf557504b6230d367c9d6606006e20455
Giuseppe Filippin <giuseppe.ferreira@pucpr.edu.br>
2025-03-07 01:49:34 -0300

    sobre mim

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -26,14 +26,12 @@ export const About = () => {
       <RevealOnScroll>
         <div className="max-w-3xl mx-auto px-4">
           <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
-            About Me
+            Sobre mim
           </h2>
           <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
             <p className="text-gray-300 mb-6">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim,
-              ullam nulla culpa et, alias fugiat nihil odit aliquid impedit
-              incidunt similique iste distinctio ducimus est hic dicta non.
-              Accusamus, deleniti!
+            Sou estudante de Ciência da Computação na PUCPR e atualmente sou bolsista de um projeto de pesquisa PIBIC na Fundação Araucária. Minha paixão pela programação me levou a explorar diversas tecnologias, do front-end ao back-end, sempre buscando aprender e aprimorar minhas habilidades.
+            Além da tecnologia, gosto de desenvolver pequenos projetos pessoais, ler sobre IA e experimentar novas stacks. Acredito que a curiosidade e a inovação são essenciais para qualquer desenvolvedor.
             </p>
             <div className="grid gird-cols-1 md:grid-cols-2 gap-6">
               <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
@@ -67,7 +65,7 @@ export const About = () => {
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
             <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
-              <h3 className="text-xl font-bold mb-4"> 📚 Education </h3>
+              <h3 className="text-xl font-bold mb-4"> 📚 Educação </h3>
               <ul className="list-disc list-inside text-gray-300 space-y-2">
                 <li>
                   <strong>English Diploma</strong> Phill Young`s
